118df3c0961a41d957df85546297c79c0018ccd5
Anna Vertegel <annavertegel@Annas-MacBook-Pro.local>
2025-12-09 11:56:52 +0200

    first commit

---
+++ sudoku/components/Board/Board.tsx
@@ -0,0 +1,12 @@
+import { Table, TableBody, TableCell, TableHeader, TableRow } from "../ui/table"
+
+export const Board = () => {
+    return (
+        <Table>
+            <TableHeader></TableHeader>
+            <TableBody>
+                <TableRow> <TableCell>1</TableCell> </TableRow>
+            </TableBody>
+        </Table>
+    )
+}
---
+++ sudoku/src/App.tsx
@@ -0,0 +1,16 @@
+import { Board } from '../components/Board/Board'
+
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Sudoku</h1>
+        <Board />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
